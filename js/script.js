b1e83fbe4a4fd6ada89881e2430f97423aa6452c
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-11-12 16:36:54 +0100

    Finished JavaScript for search bar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log(123);
+// console.log(123);
+// Script for search categories
+const select = document.getElementById("select");
+const list = document.getElementById("list");
+const selectedCategory = document.getElementById("selectedCategory");
+
+select.addEventListener("click", () => {
+  list.classList.toggle("open");
+});
+
+const categories = document.querySelectorAll(".category");
+
+categories.forEach((category) => {
+  category.addEventListener("click", () => {
+    selectedCategory.innerText = category.innerText;
+  });
+});
