033d099d7192574c5bff511eb47f78ddf0f7d463
LazanIT <117667390+LazanIT@users.noreply.github.com>
2024-01-27 20:32:10 +0100

    Started mouseover slider
    Fixing bug

--- js/script.js
+++ js/script.js
@@ -30,3 +30,32 @@ const linkHolder = document.querySelector(".link-holder ul");
 navToggle.addEventListener("click", () => {
   linkHolder.classList.toggle("show-links");
 });
+
+// Courasel
+
+const courasel = document.querySelector(".slider");
+
+let isDragingStart = false,
+  prevPageX,
+  prevScrollLeft;
+
+const dragStart = (e) => {
+  isDragingStart = true;
+  prevPageX = e.pageX;
+  prevScrollLeft = courasel.scrollLeft;
+};
+
+const draggingMouse = (e) => {
+  if (!isDragingStart) return;
+  e.preventDefault();
+  let positon = e.pageX - e.prevPageX;
+  courasel.scrollLeft = prevScrollLeft - positon;
+};
+
+const dragStop = () => {
+  isDragingStart = false;
+};
+
+courasel.addEventListener("mousedown", dragStart);
+courasel.addEventListener("mousemove", draggingMouse);
+courasel.addEventListener("mouseup", dragStop);
